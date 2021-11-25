dee59fd3647a17fe3ab2635bef77e20ecdb58b95
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-25 17:22:19 +0200

    Completed tasks

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -10,9 +10,21 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double D;
+        if (a != 0) {
+            D = b * b - 4 * a * c;
+            if (D > 0) {
+                double x1, x2;
+                x1 = (-b - Math.sqrt(D)) / (2 * a);
+                x2 = (-b + Math.sqrt(D)) / (2 * a);
+                System.out.println(x1 + " " + x2);
+            } else if (D == 0) {
+                double x;
+                x = -b / (2 * a);
+                System.out.println(x);
+            } else {
+                System.out.println("no roots");
+            }
+        }
     }
-
 }
